5bedbdff970cab7817a167c33e87a682e34ad09a
Your Name <youremail@example.com>
2025-11-20 12:24:02 +0200

    app bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,23 @@ import 'package:expense_tracker/expenses_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const ExpensesApp());
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Expense Tracker"),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        ),
+        body: ExpensesApp(),
+      ),
+    );
+  }
 }
--- lib/widgets/expenses_item.dart
+++ lib/widgets/expenses_item.dart
@@ -2,11 +2,34 @@ import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesItem extends StatelessWidget {
-  const ExpensesItem(Expense expensesList, {super.key, required this.exp});
+  const ExpensesItem({super.key, required this.exp});
   final Expense exp;
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Text(exp.title),
+            SizedBox(height: 4),
+            Row(
+              children: [
+                Text('\$${exp.amount.toStringAsFixed(2)}'),
+                Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcon[exp.category]),
+                    SizedBox(width: 8),
+                    Text(' ${exp.formattedDate}'),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
